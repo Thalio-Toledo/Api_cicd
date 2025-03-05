6b178462e8fca1ac3c3ba8dbab1a3b4e253db479
Thalio-Toledo <thalio.dev@gmail.com>
2025-03-05 11:17:08 -0300

    fix githubActions

--- obj/Debug/net8.0/API_cicd.AssemblyInfo.cs
+++ obj/Debug/net8.0/API_cicd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_cicd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddff186a1458ae4b2f0ebe4a92cf6ecacc8fc858")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_cicd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_cicd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
